cc8698488971386dbac38ac47666cf90ab430440
lsrosa <leandrors@usp.br>
2017-11-14 10:25:09 +0000

    scaling test working

--- src/adderchain/lup/ni_pipeline/config.tcl
+++ src/adderchain/lup/ni_pipeline/config.tcl
@@ -1,6 +1,11 @@
 set_combine_basicblock 2
 loop_pipeline "loop"
 #set_parameter "processor" "host"
-set_parameter LOCAL_RAMS 1
+#set_parameter LOCAL_RAMS 1
 set_parameter MODULO_SCHEDULER "NI"
 #set_parameter NI_DEBUG 1
+set_resource_constraint multiply 3
+set_resource_constraint divide 3
+set_resource_constraint altfp_add 3
+set_resource_constraint altfp_multiply 3
+set_resource_constraint altfp_divide 3
