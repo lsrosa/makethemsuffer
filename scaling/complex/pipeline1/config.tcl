3a6a75cbfb7c4804c8b498f668741d59aba48f01
leandro <leandrors@usp.br>
2018-06-05 14:52:38 -0300

    removing old stuff

---
+++ scaling/complex/pipeline1/config.tcl
@@ -0,0 +1,16 @@
+#set_parameter INCREMENTAL_SDC 1
+# resource constraints to make modulo scheduling difficult
+set_resource_constraint multiply 3
+set_resource_constraint divide 3
+set_resource_constraint altfp_add 3
+set_resource_constraint altfp_multiply 3
+set_resource_constraint altfp_divide 3
+set_operation_latency multiply 18
+
+set_parameter SDC_PRIORITY 0
+
+set_combine_basicblock 2
+loop_pipeline "loop"
+#set_parameter "processor" "host"
+#set_parameter LOCAL_RAMS 1
+set_parameter SOLVER "GUROBI"
