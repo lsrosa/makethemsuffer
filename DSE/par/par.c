b4e8fe63974d59131f0c5e759550b2b0a5fcd804
leandro <leandrors@usp.br>
2018-08-09 12:36:45 -0300

    autocommit, sorry

---
+++ DSE/par/par.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define N 100
+#define INITIALIZE {1,2,3,4,5,6,7,8,9,10, 11,12,13,14,15,16,17,18,19,20, 21,22,23,24,25,26,27,28,29,30, 31,32,33,34,35,36,37,38,39,40, 41,42,43,44,45,46,47,48,49,50, 51,52,53,54,55,56,57,58,59,60, 61,62,63,64,65,66,67,68,69,70, 71,72,73,74,75,76,77,78,79,80, 81,82,83,84,85,86,87,88,89,90, 91,92,93,94,95,96,97,98,99,-100}
+
+volatile int a[N] = INITIALIZE;
+volatile int b[N] = INITIALIZE;
+volatile int c[N] = INITIALIZE;
+volatile int d[N] = INITIALIZE;
+volatile int e[N] = INITIALIZE;
+volatile int f[N] = INITIALIZE;
+
+int main() {
+    int sum = 0;
+    int i;
+
+    loop0:  for (i = 0; i < N-1; i++) {
+      int tmp1 = a[i] * b[i];
+      int tmp2 = c[i] * d[i];
+      int tmp3 = e[i] * f[i];
+      int tmp4 = a[i+1] * b[i+1];
+      int tmp5 = c[i+1] * d[i+1];
+      int tmp6 = e[i+1] * f[i+1];
+
+      int tmp7 = tmp1/tmp2;
+      int tmp8 = tmp3/tmp4;
+      int tmp9 = tmp5/tmp6;
+
+      sum += tmp9-tmp8+tmp7;
+    }
+    printf("sum = %d\n", sum);
+
+    if (sum == 198) {
+        printf("PASSED\n");
+    } else {
+        printf("FAILED\n");
+    }
+
+    return sum;
+}
